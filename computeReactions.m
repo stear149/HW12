e3603ab05e06240459b6e1ad8284828f96bd7b87
Owen-Le-Coder <haber297@umn.edu>
2025-12-03 19:29:38 -0600

    Update computeReactions.m
    added header

--- computeReactions.m
+++ computeReactions.m
@@ -1,3 +1,25 @@
+%==========================================================================
+% function [R] = computeReactions(G,F,np,ns)
+%
+% This function takes the output arguments from getValidUserInput and uses
+% them to calculate the reactions at the nodes specified by the user.
+%
+% Input Arguments:
+% G is an N x 2 matrix, describes the geometry of the truss
+% F is an N x 2 matrix, describes the external forces acting on the truss
+% np is the primary node, as specified by the user
+% ns is the secondary node, as specified by the user
+%
+% Output Argument:
+% R is an N x 2 matrix, describes the computed reactions
+%
+% Authors: Group I
+%   Owen Haberstroh (haber297@umn.edu)
+%   Lily Wilkerson (wilke541@umn.edu)
+%   Evan Stearns (stear149@umn.edu)
+%
+% Version: Dec. 3, 2025
+%==========================================================================
 function [R] = computeReactions(G,F,np,ns)
     
     dx = G(:,1)-G(np,1);
