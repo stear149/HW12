530ae6dcc0bbf95d9d37f84bd4d5a0a3b1c8bdad
Owen-Le-Coder <haber297@umn.edu>
2025-12-03 19:41:06 -0600

    Update solveTruss.m
    added headers

--- solveTruss.m
+++ solveTruss.m
@@ -1,3 +1,25 @@
+%==========================================================================
+% function [T] = solveTruss(G,C,F,R)
+%
+% This function takes matrices output by getValidUserInput and
+% computeReactions in order to calculate the internal forces of each member
+%
+% Input Arguments:
+% G is an N x 2 matrix, describes the geometry of the truss
+% C is an M x 2 matrix, describes the start and end nodes of the members
+% F is an N x 2 matrix, describes the external forces acting on the truss
+% R is an N x 2 matrix, describes the reactions computed by computeReactions
+%
+% Output Argument:
+% T is an M x 1 matrix, describes the forces acting on each member
+%
+% Authors: Group I
+%   Owen Haberstroh (haber297@umn.edu)
+%   Lily Wilkerson (wilke541@umn.edu)
+%   Evan Stearns (stear149@umn.edu)
+%
+% Version: Dec. 3, 2025
+%==========================================================================
 function [T] = solveTruss(G,C,F,R)
 
     N = size(G,1);
