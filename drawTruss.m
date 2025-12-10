17ee62d78b97d253b2555d5cfc784e5f5704f4c3
stear149 <stear149@umn.edu>
2025-12-10 14:49:11 -0600

    Update drawTruss.m

--- drawTruss.m
+++ drawTruss.m
@@ -1,20 +1,20 @@
 %==========================================================================
 % function drawTruss(G, C, T, titlestr, radius, fontsize)
 %
-% Input Arguments:
-% G is an N x 2 matrix, describes the geometry of the truss
-% C is an M x 2 matrix, describes the start and end nodes of the members
-% T is an M x 1 matrix, describes the tenstion within the members
-% titlestr is the title of the figure
-% radius is the radius of the nodes
-% fontsize is the size of the font that numbers the nodes and describes the
-% axial forces of each member
-%
-% Output Arguments:
-% None
-%
 % This function draws a truss with the information obtained from
 % getValidUserInput to be called by visualizeTruss.
+%
+% Input Arguments:
+%   G is an N x 2 matrix, describes the geometry of the truss
+%   C is an M x 2 matrix, describes the start and end nodes of the members
+%   T is an M x 1 matrix, describes the tenstion within the members
+%   titlestr is the title of the figure
+%   radius is the radius of the nodes
+%   fontsize is the size of the font that numbers the nodes and describes the
+%   axial forces of each member
+%
+% Output Arguments:
+%   None
 %
 % Authors: Code originally written by Group G, modified by Group I
 %  Owen Haberstroh (haber297@umn.edu)
